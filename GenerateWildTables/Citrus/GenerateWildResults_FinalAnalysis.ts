f5f7d43a70c297997a5e209806ca4bf24d04abad
Steve Clark <>
2023-11-09 12:07:08 +0000

    Final analysis stage

---
+++ GenerateWildTables/Citrus/GenerateWildResults_FinalAnalysis.ts
@@ -0,0 +1,129 @@
+/**
+ * GenerateWildResults_FinalAnalysis.ts
+ *
+ * Stats file for citrus tests
+ */
+
+import { CitrusGotReelSymbolValue } from "../../games/citrus-got-reel/math/config/CitrusGotReelSymbol";
+import * as fs from "fs";
+
+import {
+	SavedWildInfo,
+	SavedResult,
+	PackScatters,
+	UnpackScatters,
+	LayoutInstance,
+	LayoutType,
+	LayoutTypeName,
+	MultiRunResult,
+	ProfileResult,
+} from "./GenerateWildResults_Defines";
+
+export const RunFinalAnalysis = function (
+	_gameInterface: any,
+	parameters: any
+) {
+	//Adjust parameters?
+
+	const adjustedParameters: any = parameters;
+
+	if (process.argv.length > 2) {
+		for (let i = 2; i < process.argv.length; i++) {
+			const arg = process.argv[i];
+		}
+	}
+
+	//Go through all result files, and build a report of the data sets
+
+	console.log("final");
+
+	//Run through all results, isolate unique instances of wild layouts (including scatters)
+
+	fs.readdir(".//GeneratedWildTablesCombined//", (err, files) => {
+		//Go through each set
+
+		for (let layoutType = 0; layoutType < LayoutType.COUNT; layoutType++) {
+			const thisSetData: any[] = [];
+
+			console.log("\nProcess layout " + LayoutTypeName[layoutType]);
+			files.forEach((file) => {
+				//console.log(file);
+
+				if (
+					file.substring(0, 11) === "runresults_" &&
+					file.substring(
+						11,
+						11 + LayoutTypeName[layoutType].length
+					) === LayoutTypeName[layoutType] &&
+					file.substring(file.length - 4) === "json"
+				) {
+					console.log("Processing file: " + file);
+
+					const thisFile: any = fs.readFileSync(
+						".//GeneratedWildTablesCombined//" + file
+					);
+
+					const thisSet: any = JSON.parse(thisFile);
+
+					//Process the set
+
+					thisSet.forEach((set) => {
+						const newEntry: any = {
+							layout: set.layout,
+							results: [],
+						};
+
+						set.results.forEach((data) => {
+							const newResult: any = {
+								profile: data.profileName,
+								averagePayout: 0,
+								averagePayoutExcluding0Win: 0,
+								chanceOfPaying: 0,
+								maxPayout: 0,
+							};
+							let total: number = 0;
+							let countNon0Win: number = 0;
+							let countOverall: number = 0;
+							data.playWins.forEach((win) => {
+								total += win[0];
+								if (win[0] !== 0) {
+									countNon0Win += win[1];
+								}
+								countOverall += win[1];
+								if (win[0] > newResult.maxPayout) {
+									newResult.maxPayout = win[0].toFixed(4);
+								}
+							});
+
+							newResult.averagePayout =
+								countOverall !== 0
+									? (total / countOverall).toFixed(4)
+									: 0;
+							newResult.averagePayoutExcluding0Win =
+								countNon0Win !== 0
+									? (total / countNon0Win).toFixed(4)
+									: 0;
+							newResult.chanceOfPaying =
+								countOverall !== 0
+									? (countNon0Win / countOverall).toFixed(4)
+									: 0;
+
+							newEntry.results.push(newResult);
+						});
+						thisSetData.push(JSON.parse(JSON.stringify(newEntry)));
+					});
+				}
+			});
+
+			//Write out
+
+			fs.writeFileSync(
+				".//GeneratedWildTablesCombined//final_" +
+					LayoutTypeName[layoutType] +
+					".json",
+				JSON.stringify(thisSetData),
+				{ encoding: "utf8", flag: "w" }
+			);
+		}
+	});
+};
