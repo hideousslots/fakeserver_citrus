9824b65d9ed07de79235ca8e3c15d332c18bd0a0
Steve Clark <>
2023-11-18 06:30:15 +0000

    Run results for layouts 1025-2047 inclusive

---
+++ GenerateWildResults_UseAll_1025-2047.bat
@@ -0,0 +1,101 @@
+@rem Create a set of results
+
+@call GenerateWildResults_UseSingle.bat 0 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 0 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 0 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 0 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 1 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 1 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 1 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 1 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 2 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 2 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 2 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 2 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 3 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 3 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 3 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 3 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 4 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 4 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 4 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 4 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 5 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 5 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 5 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 5 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 6 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 6 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 6 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 6 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 7 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 7 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 7 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 7 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 8 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 8 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 8 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 8 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 9 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 9 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 9 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 9 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 10 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 10 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 10 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 10 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 11 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 11 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 11 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 11 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 12 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 12 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 12 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 12 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 13 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 13 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 13 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 13 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 14 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 14 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 14 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 14 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 15 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 15 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 15 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 15 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 16 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 16 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 16 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 16 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 17 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 17 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 17 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 17 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 18 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 18 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 18 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 18 1000 1792 2047
+
+@call GenerateWildResults_UseSingle.bat 19 1000 1025 1279
+@call GenerateWildResults_UseSingle.bat 19 1000 1280 1535
+@call GenerateWildResults_UseSingle.bat 19 1000 1536 1791
+@call GenerateWildResults_UseSingle.bat 19 1000 1792 2047
